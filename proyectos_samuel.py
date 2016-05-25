abf2ff22cc00330c05e5ec8a54b60a1c0d21720b
colombia23 <candres125@hotmail.es>
2016-05-25 07:49:07 -0600

    agregue el prob4

---
+++ proyectos_samuel.py
@@ -0,0 +1,18 @@
+def proble1 (num):
+	if proble1 % = 2:
+		print ("el numero es par")
+	else:
+		print("el numero es impar")
+proble1(9)		
+	
+		
+def proble2 (n):
+	convert = (n - 32) * 5/9
+	print(convert)
+
+
+def prob_3(a,b):
+	operar = a**b
+	print(operar)
+
+
